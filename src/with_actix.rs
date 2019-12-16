af677076eba21d198ec1f422998103c1ecb37aa5
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-12-16 16:16:57 +0100

    wip

--- src/with_actix.rs
+++ src/with_actix.rs
@@ -1,10 +1,12 @@
 use std::{error::Error, fmt};
+//
 use std::pin::Pin;
 use futures::future::Future;
 
 use crate::{I18n, Translations, ACCEPT_LANG};
 
 use actix_web::{dev::Payload, FromRequest, HttpRequest, ResponseError};
+use actix_web::http::header::HeaderValue;
 
 #[derive(Debug)]
 pub struct MissingTranslationsError(String);
@@ -47,41 +49,45 @@ impl ResponseError for MissingStateError {
 impl FromRequest for I18n {
     type Config = ();
     type Error = actix_web::Error;
-    // type Future = Result<Self, Self::Error>;
-    type Future = Pin<Box<dyn Future<Output = Result<Self, Error>>>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
 
-    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        // let langs = req.app_data::<Translations>().ok_or(MissingStateError)?;
-        let langs = req.app_data::<Translations>();
+    fn from_request<'request>(req: &'request HttpRequest, _: &mut Payload) -> Self::Future {
 
-        let lang_pre = req
+        // let accept_lang = "en"; 
+        let accept_lang = req
             .headers()
             .get(ACCEPT_LANG)
-            .and_then(|v| v.to_str().ok())
-            .unwrap_or("en")
-            .split(",")
+        //     // .await
+            .unwrap_or(&HeaderValue::from_static("en"))
+            .to_str().ok()
+            .unwrap_or("en");
+        let langs = req.app_data::<Translations>().ok_or(MissingStateError);
+        let langs = match langs {
+                Ok(langs) => langs.clone(),
+                e => return Box::pin(async {Err(MissingStateError.into())})
+        };
+        let lang = accept_lang.split(",")
             .filter_map(|lang| {
                 lang
                     // Get the locale, not the country code
                     .split(|c| c == '-' || c == ';')
                     .nth(0)
-            });
-
-        Box::pin(async move {
-            let langs = langs.ok_or(MissingStateError)?;
-            let lang = lang_pre
+            })
             // Get the first requested locale we support
             .find(|lang| langs.iter().any(|l| l.0 == &lang.to_string()))
-            .unwrap_or("en");
+                .unwrap_or("en");
+
+        Box::pin( async move {
 
             match langs.iter().find(|l| l.0 == lang) {
-                Some(translation) => 
-                    Ok(I18n {
-                        catalog: translation.1.clone(),
-                        lang: translation.0,
-                    }),
+                Some(translation) => Ok(I18n {
+                    catalog: translation.1.clone(),
+                    lang: translation.0,
+                }),
                 None => Err(MissingTranslationsError(lang.to_owned()).into()),
             }
-        })
+        }
+        )
     }
+
 }
