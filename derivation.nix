67b33cf3902cb985cf070773d214ee0eb7fb4663
Henri Bourcereau <hbourcereau@atixnet.fr>
2020-01-14 18:10:04 +0100

    wip nix packaging

---
+++ derivation.nix
@@ -0,0 +1,41 @@
+# { lib, rustPlatform }:
+# rustPlatform.buildRustPackage rec {
+{ stdenv, lib, makeRustPlatform, fetchFromGitHub, pkgs }:
+let
+  mozRepo = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "b5f2af80f16aa565cef33d059f27623d258fef67";
+    sha256 = "0s552nwnxcn6nnzrqaazhdgx5mm42qax9wy1gh5n6mxfaqi6dvbr";
+  };
+  # `mozPkgs` is the package set of `mozRepo`; this differs from their README
+  # where they use it as an overlay rather than a separate package set
+  mozPkgs = import "${mozRepo}/package-set.nix" { inherit pkgs; };
+  channel = mozPkgs.rustChannelOf { date = "2019-11-29"; channel = "nightly"; };
+  nightlyRustPlatform = makeRustPlatform {
+    rustc = channel.rust;
+    cargo = channel.cargo;
+  };
+in
+
+nightlyRustPlatform.buildRustPackage rec {
+# stdenv.mkDerivation rec {
+  pname = "actix_i18n";
+  version = "0.6.0";
+  cargoSha256 = "1kbd76z3kfyqmkc2170n2lpbfnam9gn2fcz69mqxdsgqzw50pmsi";
+  src = ./.;
+
+  # buildPhase = ''
+  #   # prevent referring /homeless-shelter
+  #   export HOME=$(pwd)
+  #   ${channel.cargo}/bin/cargo build  
+  #   '';
+  # installPhase = ''
+  #   ${channel.cargo}/bin/cargo install
+  #   '';
+
+  meta = {
+    description = "Books management";
+    maintainers = with lib.maintainers; [ mmai ];
+  };
+}
